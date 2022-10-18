cec89fddcb9b2c1b0f6e90e2bb53222d3ec09366
officialesse <uzomaesse@gmail.com>
2022-10-18 23:29:45 +0100

    feat made breaking changeS

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -11,7 +11,7 @@ const Customers = () => {
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Customers" />
+      <Header category="Page" title="Student" />
       <GridComponent
         dataSource={customersData}
         enableHover={false}
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -11,7 +11,7 @@ const Employees = () => {
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Employees" />
+      <Header category="Page" title="teacher" />
       <GridComponent
         dataSource={employeesData}
         width="auto"
