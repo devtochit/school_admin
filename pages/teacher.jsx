1886444672c405421583c133cc9bf4181d7c6446
hedevedup <uzomaesse@gmail.com>
2022-10-28 09:43:58 +0100

    feat:changed ui

--- pages/teacher.jsx
+++ pages/teacher.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import TeacherPage from '../scenes/teacher/index';
- import  TeacherButton from '../scenes/teacherButton/index'
-const Teacher = () => {
-    return (
-     <>
-        <TeacherButton />
-       <TeacherPage />
-     </>
-    );
-}
+// import React from 'react';
+// import TeacherPage from '../scenes/teacher/index';
+//  import  TeacherButton from '../scenes/teacherButton/index'
 
-export default Teacher;
+// const Teacher = () => {
+//     return (
+//      <>
+//         {/* <TeacherButton /> */}
+//        <TeacherPage />
+//      </>
+//     );
+// }
+
+// export default Teacher;
