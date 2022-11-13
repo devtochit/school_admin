8f172a6f18dfbf328cabdbc77f913fdbcb8194d5
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-11-13 05:55:36 -0700

    feat: added changes to student screen

--- pages/teacher.jsx
+++ pages/teacher.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TeacherPage from '../scenes/teacher/index';
- import  TeacherButton from '../scenes/teacherButton/index'
+
 
 const Teacher = () => {
     return (
